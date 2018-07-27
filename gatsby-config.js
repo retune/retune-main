ea6df7e6d7e8d867b88ac492c21019f8c2a693b8
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-07-27 18:04:14 +0200

    Connect to Cockpit for fetching data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,21 @@ module.exports = {
   siteMetadata: {
     title: 'Retune',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-postcss'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-plugin-cockpit',
+      options: {
+        cockpitConfig: {
+          baseURL: 'https://retune.uber.space',
+          folder: '/cockpit-master',
+          accessToken: '6b12ce26d8ef977bcb592e0bcfe6cd',
+          collections: null /* fetch all */,
+          regions: null /* fetch all */,
+          //customComponents: [],
+        },
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Intro from '../components/Intro'
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
+    <h1>{data.tagline.values.tagline}</h1>
     <Intro />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    tagline: region(name: { eq: "homepage" }) {
+      values {
+        tagline
+      }
+    }
+  }
+`
 
 export default IndexPage
