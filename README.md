0ca993d74f0d9c0b2be45c6cbca915fd756dc9fd
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-08-03 13:10:22 +0200

    Masthead feature content (from CMS)

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Deployment
 
 https://oncletom.io/2016/travis-ssh-deploy/
+
+## Content
+
+| Description              | CMS Location                                  |
+| ------------------------ | --------------------------------------------- |
+| Masthead: feature banner | Region -> masthead -> featureText, featureURL |
--- src/components/Masthead/index.js
+++ src/components/Masthead/index.js
@@ -1,5 +1,29 @@
 import * as React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
 import styles from './index.module.css'
 
-export default () => <header className={styles.Masthead}>Retune</header>
+export default () => (
+  <StaticQuery
+    query={graphql`
+      {
+        masthead: region(name: { eq: "masthead" }) {
+          values {
+            featureText
+            featureURL
+          }
+        }
+      }
+    `}
+    render={data => (
+      <header className={styles.Masthead}>
+        <div className={styles.site}>Retune</div>
+        <div className={styles.feature}>
+          <a href={data.masthead.values.featureURL}>
+            {data.masthead.values.featureText}
+          </a>
+        </div>
+      </header>
+    )}
+  />
+)
