78fb8dd064212b4405234c5a965aa9126570803f
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-08-03 18:03:39 +0200

    Adds plugins to match v1 gatsby

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 const postcssImport = require('postcss-import')
 const postcssCustomMedia = require('postcss-custom-media')
+const postcssPresetEnv = require('postcss-preset-env')
+const postcssBrowserReporter = require(`postcss-browser-reporter`)
+const postcssReporter = require(`postcss-reporter`)
 
 module.exports = {
-  plugins: [postcssImport(), postcssCustomMedia()],
+  plugins: [
+    // postcssImport(),
+    // postcssPresetEnv(),
+    // postcssCustomMedia(),
+    // postcssBrowserReporter(),
+    // postcssReporter(),
+  ],
 }
