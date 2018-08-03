90e75cb80bed73306f45ce3768244bf47e7eae04
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-08-03 18:25:11 +0200

    PostCSS configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
 const postcssImport = require('postcss-import')
-const postcssCustomMedia = require('postcss-custom-media')
 const postcssPresetEnv = require('postcss-preset-env')
+const postcssCustomMedia = require('postcss-custom-media')
 const postcssBrowserReporter = require(`postcss-browser-reporter`)
 const postcssReporter = require(`postcss-reporter`)
 
 module.exports = {
   plugins: [
-    // postcssImport(),
-    // postcssPresetEnv(),
-    // postcssCustomMedia(),
-    // postcssBrowserReporter(),
-    // postcssReporter(),
+    postcssImport(),
+    postcssPresetEnv(),
+    postcssCustomMedia(),
+    postcssBrowserReporter(),
+    postcssReporter(),
   ],
 }
