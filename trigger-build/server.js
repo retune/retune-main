4a58c0f19879bd75c94b3358ecf55c17bb5cc782
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-08-04 09:18:21 +0200

    Tiny server that listens for webhooks and triggers travis CI build for deployment

---
+++ trigger-build/server.js
@@ -0,0 +1,53 @@
+const fetch = require('node-fetch')
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+
+const triggerTravisBuild = () => {
+  const apiToken = fs.readFileSync(
+    path.join(__dirname, '/retune-travis-api-token.txt')
+  )
+
+  if (!apiToken) {
+    console.error('API token cannot be found')
+    return
+  }
+
+  const org = 'retune'
+  const repo = 'retune-main'
+
+  const url = `https://api.travis-ci.com/repo/${org}%2F${repo}/requests`
+
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    'Travis-API-Version': '3',
+    Authorization: `token ${apiToken}`,
+  }
+
+  const body = {
+    request: {
+      branch: 'master',
+    },
+  }
+
+  return fetch(url, { method: 'POST', headers, body: JSON.stringify(body) })
+}
+
+app.get('/', (req, res) => res.send('UP'))
+app.post('/hook', async (req, res) => {
+  triggerTravisBuild().then(apiResponse => {
+    if (apiResponse.ok) {
+      res.json({ ok: true }).end()
+    } else {
+      res.json({ bad: true }).end()
+    }
+  })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () =>
+  console.log(`Webhook receiver listening on port ${port}`)
+)
