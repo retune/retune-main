6c1af9eb5d2605a12187f24f04ed813d270aaa83
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-08-09 18:44:18 +0200

    Extracts and displays featured events from CMS

---
+++ src/components/Featured/index.js
@@ -0,0 +1,28 @@
+//@flow
+import * as React from 'react'
+
+import EventType from '../EventType'
+
+import styles from './index.module.css'
+
+const Event = ({ event }) => (
+  <li className={styles.Event}>
+    <p className={styles.type}>
+      <EventType plural type={event.type} />
+    </p>
+    <div className={styles.image} />
+    <h4 className={styles.title}>
+      <a href="">{event.title}</a>
+    </h4>
+    <p className={styles.summary}>{event.summary}</p>
+  </li>
+)
+
+export default ({ events }) => (
+  <section className={styles.Featured}>
+    <h3 className={styles.heading}>Featured</h3>
+    <ul className={styles.list}>
+      {events.map(event => <Event key={event.id} event={event} />)}
+    </ul>
+  </section>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import find from 'lodash/find'
+import map from 'lodash/map'
 
 import Intro from '../components/Intro'
 import Latest from '../components/Latest'
 import Layout from '../components/Layout'
 import Promo from '../components/Promo'
+import Featured from '../components/Featured'
+
+const findEventsByIds = (events, ids) => map(ids, id => find(events, { id }))
 
 const mergeResultsIntoItems = results => results.edges.map(edge => edge.node)
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <Intro />
-    <Latest items={mergeResultsIntoItems(data.events)} />
-    {data.homepage.values.showPromo && (
-      <Promo url={data.homepage.values.promoURL} />
-    )}
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const events = mergeResultsIntoItems(data.events)
+  const featuredIds = map(data.homepage.values.featured, 'id')
+  const featuredEvents = findEventsByIds(events, featuredIds)
+
+  return (
+    <Layout>
+      <Intro />
+      <Latest items={events} />
+      {data.homepage.values.showPromo && (
+        <Promo url={data.homepage.values.promoURL} />
+      )}
+      <Featured events={featuredEvents} />
+    </Layout>
+  )
+}
 
 export const query = graphql`
   {
@@ -24,6 +36,9 @@ export const query = graphql`
       values {
         showPromo
         promoURL
+        featured {
+          id: _id
+        }
       }
     }
     events: allEvent {
@@ -33,6 +48,7 @@ export const query = graphql`
           type
           title
           subtitle
+          summary
           startDate
           startTime
           endDate
