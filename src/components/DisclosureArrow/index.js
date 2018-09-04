426a24ac778df0565710690ae264028f34f7dc98
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-09-04 22:47:44 +0200

    Disclosure bling

--- src/components/DisclosureArrow/index.js
+++ src/components/DisclosureArrow/index.js
@@ -10,15 +10,24 @@ const DisclosureArrow = ({
   isOpen = false,
   size = 'large',
 }) => (
-  <Icon
+  <svg
     className={classnames(
       styles.disclosureArrow,
       styles[size],
       isOpen ? styles.isOpen : styles.isClosed,
       className
     )}
-    type="disclosureArrow"
-  />
+    xmlns="http://www.w3.org/2000/svg"
+    width="50"
+    height="45"
+    viewBox="0 0 50 45"
+  >
+    <polygon
+      fill="#D8D8D8"
+      points="25 0 50 45 0 45"
+      transform="rotate(180 25 22.5)"
+    />
+  </svg>
 )
 
 export default DisclosureArrow
