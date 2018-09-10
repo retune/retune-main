88527be44bde7f903902c00ce15eff85618d6dc5
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-09-10 09:08:44 +0100

    Process images during build

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,8 @@ module.exports = {
     title: 'Retune',
   },
   plugins: [
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-postcss',
     {
--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,19 +1,42 @@
 import * as React from 'react'
 import get from 'lodash/get'
 import classnames from 'classnames'
+import Img from 'gatsby-image'
 
 import styles from './index.module.css'
 
-const Image = ({ className = '', source, width, height, auto = false }) => {
+const Image = ({
+  className = '',
+  position,
+  source,
+  width,
+  height,
+  auto = false,
+}) => {
+  const classes = classnames(styles.Image, className)
   const style = auto ? null : { width, height }
+  const gatsbyImage = get(source, 'localFile.childImageSharp', null)
 
-  return (
-    <div className={classnames(styles.Image, className)} style={style}>
-      {get(source, 'localFile.publicURL') ? (
+  let image = null
+
+  // Using gatsby's image processing
+  if (gatsbyImage && gatsbyImage.fixed) {
+    image = (
+      <Img className={classes} style={{ position }} fixed={gatsbyImage.fixed} />
+    )
+  } else if (gatsbyImage && gatsbyImage.fluid) {
+    image = (
+      <Img className={classes} style={{ position }} fluid={gatsbyImage.fluid} />
+    )
+  } else if (get(source, 'localFile.publicURL')) {
+    image = (
+      <div className={classes} style={style}>
         <img src={get(source, 'localFile.publicURL')} />
-      ) : null}
-    </div>
-  )
+      </div>
+    )
+  }
+
+  return image
 }
 
 export default Image
