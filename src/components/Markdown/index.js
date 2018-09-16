07296897f41778896d6d1c893e7aaf3a995663b0
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-09-16 13:51:54 +0200

    Allows HTML to be parsed in Markdown fields

--- src/components/Markdown/index.js
+++ src/components/Markdown/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import ReactMarkdown from 'react-markdown'
 
-const Markdown = props => <ReactMarkdown {...props} />
+const Markdown = props => <ReactMarkdown escapeHtml={false} {...props} />
 
 export default Markdown
