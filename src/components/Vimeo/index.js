a3c1e2a27dcdaec8af4516d255ec07dba394efed
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-09-18 10:20:23 +0200

    Flexible Vimeo video

--- src/components/Vimeo/index.js
+++ src/components/Vimeo/index.js
@@ -1,15 +1,25 @@
 import * as React from 'react'
 
 const Vimeo = ({ id, width = '500', height = '281' }) => (
-  <iframe
-    src={`https://player.vimeo.com/video/${id}`}
-    width={width}
-    height={height}
-    frameborder="0"
-    webkitallowfullscreen
-    mozallowfullscreen
-    allowfullscreen
-  />
+  <React.Fragment>
+    <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+      <iframe
+        src={`https://player.vimeo.com/video/${id}`}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        }}
+        frameborder="0"
+        webkitallowfullscreen
+        mozallowfullscreen
+        allowfullscreen
+      />
+    </div>
+    <script src="https://player.vimeo.com/api/player.js" />
+  </React.Fragment>
 )
 
 export default Vimeo
