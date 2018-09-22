cf8081f6dad4be4c193cc4fe7a26b09d832bd039
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-09-22 13:59:16 +0200

    Upgrades to latest gatsby release (and fixes issue with images)

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -2,17 +2,11 @@ import * as React from 'react'
 import get from 'lodash/get'
 import classnames from 'classnames'
 import Img from 'gatsby-image'
+import { graphql } from 'gatsby'
 
 import styles from './index.module.css'
 
-const Image = ({
-  className = '',
-  position,
-  source,
-  width,
-  height,
-  auto = false,
-}) => {
+const Image = ({ className = '', source, width, height, auto = false }) => {
   const classes = classnames(styles.Image, className)
   const style = auto ? null : { width, height }
   const gatsbyImage = get(source, 'localFile.childImageSharp', null)
@@ -21,13 +15,9 @@ const Image = ({
 
   // Using gatsby's image processing
   if (gatsbyImage && gatsbyImage.fixed) {
-    image = (
-      <Img className={classes} style={{ position }} fixed={gatsbyImage.fixed} />
-    )
+    image = <Img className={classes} fixed={gatsbyImage.fixed} />
   } else if (gatsbyImage && gatsbyImage.fluid) {
-    image = (
-      <Img className={classes} style={{ position }} fluid={gatsbyImage.fluid} />
-    )
+    image = <Img className={classes} fluid={gatsbyImage.fluid} />
   } else if (get(source, 'localFile.publicURL')) {
     image = (
       <div className={classes} style={style}>
