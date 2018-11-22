da077726f832ed3607d72d1d837e6ec5b0bf0bc6
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-11-22 15:50:16 +0100

    Retune-style date format

--- src/components/FormattedDate/index.js
+++ src/components/FormattedDate/index.js
@@ -1,7 +1,6 @@
 import { DateTime } from 'luxon'
 
-export const formatDate = date =>
-  DateTime.fromISO(date).toLocaleString(DateTime.DATE_SHORT)
+export const formatDate = date => DateTime.fromISO(date).toFormat('dd.LL.yyyy')
 
 const FormattedDate = ({ date, time }) => formatDate(date)
 
