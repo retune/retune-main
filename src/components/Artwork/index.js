3d7ce5d0ae5a366a2345889fd5ac0d8073457d61
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2019-01-17 12:05:24 +0100

    Adds animated gradient for start page Artwork

--- src/components/Artwork/index.js
+++ src/components/Artwork/index.js
@@ -1,10 +1,56 @@
 import * as React from 'react'
+import Granim from 'granim'
 
 import styles from './index.module.css'
 
 class Artwork extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.ref = React.createRef()
+  }
+
+  componentDidMount() {
+    if (this.ref.current == null) {
+      return
+    }
+
+    this.instance = new Granim({
+      element: this.ref.current,
+      name: 'granim',
+      isPausedWhenNotInView: true,
+      opacity: [1, 1],
+      direction: 'top-bottom',
+      defaultStateName: 'horizontal',
+      states: {
+        horizontal: {
+          gradients: [
+            [
+              { color: '#000096', pos: 0.07 },
+              { color: '#00FCC9', pos: 0.52 },
+              { color: '#000597', pos: 1 },
+            ],
+            [
+              { color: '#000096', pos: 0.07 + 0.1 },
+              { color: '#00FCC9', pos: 0.52 + 0.1 },
+              { color: '#000597', pos: 1 },
+            ],
+          ],
+        },
+      },
+    })
+  }
+
+  componentWillUnmount() {
+    this.instance.destroy()
+  }
+
   render() {
-    return <div className={styles.Artwork} />
+    return (
+      <div className={styles.Artwork}>
+        <canvas ref={this.ref} />
+      </div>
+    )
   }
 }
 
