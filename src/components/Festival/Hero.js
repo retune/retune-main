a0d3869f878d890aa77f67a4c360730bc9a549a9
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2019-02-28 12:53:21 +0100

    Border widths

--- src/components/Festival/Hero.js
+++ src/components/Festival/Hero.js
@@ -9,16 +9,20 @@ import styles from './Hero.module.css'
 const FestivalHero = ({ event }) => (
   <div className={classnames(styles.Hero)}>
     <div className={styles.titles}>
-      <h2 className={classnames('mql-xl', styles.title)}>{event.title}</h2>
-      <p className={classnames(styles.subtitle, 'mql-xl')}>{event.subtitle}</p>
+      <h2 className={classnames('mql-xl mqs-xl', styles.title)}>
+        {event.title}
+      </h2>
+      <p className={classnames(styles.subtitle, 'mql-xl mqs-xl')}>
+        {event.subtitle}
+      </p>
     </div>
-    <div className={styles.image}>
-      {event.mainImages && <Flipbook images={event.mainImages} />}
+    <div className={styles.images}>
+      {event.mainimages && <Flipbook images={event.mainimages} />}
     </div>
-    <div className={classnames(styles.body, 'mql-s mono')}>
+    <div className={classnames(styles.body, 'mql-s mqs-m mono')}>
       <Markdown source={event.description} />
     </div>
-    <p className={classnames(styles.link, 'mql-xl')}>
+    <p className={classnames(styles.link, 'mql-xl mqs-xl')}>
       ({' '}
       <span>
         -&gt;{' '}
