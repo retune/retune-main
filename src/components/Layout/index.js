08feaf0eb8bbf16e10c30b4cb30eddf3fb205f4d
julianadenauer <github@julianadenauer.de>
2019-03-18 11:02:40 +0100

    added favicon to repository

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -18,7 +18,7 @@ import { extractImageUrl } from '../Image'
 import './index.css'
 import styles from './index.module.css'
 
-import favicon from './favicon.png'
+import favicon from 'favicon.png'
 
 class Layout extends React.Component {
   state = {
